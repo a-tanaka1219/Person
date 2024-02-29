a6efa5a79ab9be365edf4e82e6c35945e4b0cbfe
田中彩美紗 <amisasm@tanakaayamishanoMacBook-Air.local>
2024-02-29 21:54:57 +0900

    演習２

--- src/Test.java
+++ src/Test.java
@@ -7,5 +7,28 @@ public class Test{
 		
 		System.out.println(taro.name);
 		System.out.println(taro.age);
+		
+		Person jiro = new Person();
+		jiro.name="木村次郎";
+		jiro.age=18;
+		
+		System.out.println(jiro.name);
+		System.out.println(jiro.age);
+		
+		Person hanako = new Person();
+		hanako.name="鈴木花子";
+		hanako.age=16;
+		
+		System.out.println(hanako.name);
+		System.out.println(hanako.age);
+		
+		Person amisa = new Person();
+		amisa.name="田中彩美紗";
+		amisa.age=26;
+		
+		System.out.println(amisa.name);
+		System.out.println(amisa.age);
 	}
+	
+	
 }
